bc97ff3c408c4367358fea37e4f812372d1de836
weijuer <weijuer@192>
2016-02-18 23:29:34 +0800

    mySQL-user

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -69,7 +69,7 @@ return array(
 	'DB_TYPE'   => 'mysql', // 数据库类型
 	'DB_HOST'   => '127.0.0.1', // 服务器地址
 	'DB_NAME'   => 'test', // 数据库名
-	'DB_USER'   => 'demo', // 用户名
+	'DB_USER'   => 'weijuer', // 用户名
 	'DB_PWD'    => 'nj123456', // 密码
 	'DB_PORT'   => 3306, // 端口
 	'DB_PREFIX' => 'weijuer_', // 数据库表前缀 
